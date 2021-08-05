c2c74806e572f84bfc8e930552739e101c98f972
moonheee <enduser0@naver.com>
2021-08-05 10:59:55 +0900

    삭제 할께

--- reviewapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('review_list/', views.review_list, name='review_list'),
-    path('review_detail/', views.review_detail, name='review_detail'),
-]
